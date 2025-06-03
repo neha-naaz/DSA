2e4caee00629a811beac4e70a5b9e58e0b440bd3
neha-naaz <97825754+neha-naaz@users.noreply.github.com>
2025-06-03 23:29:32 +0530

    Update LowestCommonAncestor.java

--- Trees/LowestCommonAncestor.java
+++ Trees/LowestCommonAncestor.java
@@ -53,4 +53,19 @@ class LowestCommonAncestor {
             return root;
         }
     }
+    // Iterative version
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode cur = root;
+        
+        while (cur != null) {
+            if (p.val > cur.val && q.val > cur.val) {
+                cur = cur.right;
+            } else if (p.val < cur.val && q.val < cur.val) {
+                cur = cur.left;
+            } else {
+                return cur;
+            }
+        }
+        return null;
+    }
 }
