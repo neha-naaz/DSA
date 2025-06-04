412e41db99d0ecae18c20938f6aaeb86db6a826b
neha-naaz <97825754+neha-naaz@users.noreply.github.com>
2025-06-04 22:53:44 +0530

    Create ValidBST.java

---
+++ Trees/ValidBST.java
@@ -0,0 +1,58 @@
+/*
+Given the root of a binary tree, return true if it is a valid binary search tree, otherwise return false.
+A valid binary search tree satisfies the following constraints:
+
+The left subtree of every node contains only nodes with keys less than the node's key.
+The right subtree of every node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees are also binary search trees.
+
+Example 1:
+Input: root = [2,1,3]
+Output: true
+
+Example 2:
+Input: root = [1,2,3]
+Output: false
+Explanation: The root node's value is 1 but its left child's value is 2 which is greater than 1.
+
+Constraints:
+1 <= The number of nodes in the tree <= 1000.
+-1000 <= Node.val <= 1000
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class ValidBST {
+    public boolean isValidBST(TreeNode root) {
+        if(root == null) {
+            true;
+        }
+
+        return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private dfs(TreeNode root, int left, int right) {
+        if(root == null) {
+            return true;
+        }
+        if(!(left < root.val && root.val < right)) {
+            return false;
+        }
+        return dfs(root.left, left, root.val) &&
+               dfs(root.right, root.val, right);
+    }
+}
