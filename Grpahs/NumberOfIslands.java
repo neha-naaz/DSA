97da0d3af7cf6d233388a49191b7c2b01614a56b
neha-naaz <97825754+neha-naaz@users.noreply.github.com>
2025-07-26 22:47:54 +0530

    Update NumberOfIslands.java

--- Grpahs/NumberOfIslands.java
+++ Grpahs/NumberOfIslands.java
@@ -1,10 +1,9 @@
 /*
 Given a 2D grid grid where '1' represents land and '0' represents water, count and return the number of islands.
-
-An island is formed by connecting adjacent lands horizontally or vertically and is surrounded by water. You may assume water is surrounding the grid (i.e., all the edges are water).
+An island is formed by connecting adjacent lands horizontally or vertically and is surrounded by water. 
+You may assume water is surrounding the grid (i.e., all the edges are water).
 
 Example 1:
-
 Input: grid = [
     ["0","1","1","1","0"],
     ["0","1","0","1","0"],
@@ -12,8 +11,8 @@ Input: grid = [
     ["0","0","0","0","0"]
   ]
 Output: 1
-Example 2:
 
+Example 2:
 Input: grid = [
     ["1","1","0","0","1"],
     ["1","1","0","0","1"],
@@ -21,8 +20,8 @@ Input: grid = [
     ["0","0","0","1","1"]
   ]
 Output: 4
-Constraints:
 
+Constraints:
 1 <= grid.length, grid[i].length <= 100
 grid[i][j] is '0' or '1'.
 */
